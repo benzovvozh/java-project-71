48628ef7465872e733173ce928f16973a5c1c538
Vladimir Shumkov <augusthoffman3@gmail.com>
2024-11-07 16:25:44 +0500

    add picocli

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("java")
+    application
     checkstyle
     jacoco
 }
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation ("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
-public class App {
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+    @Option(
+            names = {"-f", "--format"},
+            defaultValue = "stylish",
+            paramLabel = "format",
+            description = "output format [default: ${DEFAULT-VALUE}]"
+    )
+    private String format;
+    @Parameters(paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+    @Parameters(paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+    @Override
+    public Integer call() throws Exception {
+        return null;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
     }
 }
