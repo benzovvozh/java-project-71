500a866bf1778fd1b859f510bbfa0cda3ce265c9
Vladimir Shumkov <augusthoffman3@gmail.com>
2024-11-10 13:15:26 +0500

    add first test

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,26 @@
+
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void call() throws Exception {
+        String expected = "{\n"
+                + "  - follow: false\n"
+                + "    host: hexlet.io\n"
+                + "  - proxy: 123.234.53.22\n"
+                + "  - timeout: 50\n"
+                + "  + timeout: 20\n"
+                + "  + verbose: true\n"
+                + "}";
+        String actual = Differ.generate("file1.json", "file2.json", "stylish");
+        assertEquals(expected, actual);
+    }
+
+
+}
+
