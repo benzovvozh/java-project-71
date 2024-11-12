9203d3bf283fdee5bacd4edc6695d251badb26a4
Vladimir Shumkov <augusthoffman3@gmail.com>
2024-11-12 23:38:15 +0500

    add tests and test.txt files

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -1,14 +1,35 @@
 package hexlet.code.formatters;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class Json {
-    public static String format(List<Map<String, Object>> result) throws JsonProcessingException {
+    public static String format(List<Map<String, Object>> result) throws Exception {
         ObjectMapper mapperJson = new ObjectMapper();
-        return mapperJson.writeValueAsString(result);
+
+        return mapperJson.writeValueAsString(formattedList(result));
+    }
+
+    public static List<Map<String, Object>> formattedList(List<Map<String, Object>> result) throws Exception {
+        List<Map<String, Object>> formatted = new ArrayList<>();
+        for (Map<String, Object> item : result) {
+            Map<String, Object> map = new LinkedHashMap<>();
+            // обходим каждую мапу
+            // перебираем каждую пару ключ-значение в карте
+            for (Map.Entry<String, Object> entry : item.entrySet()) {
+                var key = entry.getKey();
+                var value = entry.getValue();
+                String formattedKey = key.substring(3);
+                map.put(formattedKey, value);
+            }
+            formatted.add(map);
+        }
+
+        return formatted;
     }
 }
--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -1,74 +1,64 @@
 
 package hexlet.code;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class AppTest {
+    private static String excepted1;
+    private static String excepted2;
+    private static String excepted3;
+    private static String excepted4;
+
+    @BeforeAll
+    public static void beforeAll() throws IOException {
+
+        excepted1 = new String(Files.readString(Paths.get("src/test/resources/callTest.txt")
+                .toAbsolutePath().normalize()));
+        excepted2 = new String(Files.readString(Paths.get("src/test/resources/callYamlTest.txt")
+                .toAbsolutePath().normalize()));
+        excepted3 = new String(Files.readString(Paths.get("src/test/resources/callPlainTest.txt")
+                .toAbsolutePath().normalize()));
+        excepted4 = new String(Files.readString(Paths.get("src/test/resources/callJsonTest.txt")
+                .toAbsolutePath().normalize()));
+    }
 
     @Test
-    void callJson() throws Exception {
-        String expected = "{\n"
-                + "    chars1: [a, b, c]\n"
-                + "  - chars2: [d, e, f]\n"
-                + "  + chars2: false\n"
-                + "  - checked: false\n"
-                + "  + checked: true\n"
-                + "  - default: null\n"
-                + "  + default: [value1, value2]\n"
-                + "  - id: 45\n"
-                + "  + id: null\n"
-                + "  - key1: value1\n"
-                + "  + key2: value2\n"
-                + "    numbers1: [1, 2, 3, 4]\n"
-                + "  - numbers2: [2, 3, 4, 5]\n"
-                + "  + numbers2: [22, 33, 44, 55]\n"
-                + "  - numbers3: [3, 4, 5]\n"
-                + "  + numbers4: [4, 5, 6]\n"
-                + "  + obj1: {nestedKey=value, isNested=true}\n"
-                + "  - setting1: Some value\n"
-                + "  + setting1: Another value\n"
-                + "  - setting2: 200\n"
-                + "  + setting2: 300\n"
-                + "  - setting3: true\n"
-                + "  + setting3: none\n"
-                + "}";
+    void call() throws Exception {
+        String expected = excepted1;
         String actual = Differ.generate("file1.json", "file2.json", "");
-        assertEquals(expected, actual);
+        assertEquals(expected.trim(), actual.trim());
     }
 
     @Test
     void callYaml() throws Exception {
-        String expected = "{\n"
-                + "  - follow: false\n"
-                + "    host: hexlet.io\n"
-                + "  - proxy: 123.234.53.22\n"
-                + "  - timeout: 50\n"
-                + "  + timeout: 20\n"
-                + "  + verbose: true\n"
-                + "}";
+        String expected = excepted2;
         String actual = Differ.generate("file1.yaml", "file2.yaml", "stylish");
-        assertEquals(expected, actual);
+        assertEquals(expected.trim(), actual.trim());
     }
 
     @Test
     void callPlain() throws Exception {
-        String expected = "Property 'chars2' was updated. From [complex value] to false\n"
-                + "Property 'checked' was updated. From false to true\n"
-                + "Property 'default' was updated. From null to [complex value]\n"
-                + "Property 'id' was updated. From 45 to null\n"
-                + "Property 'key1' was removed\n"
-                + "Property 'key2' was added with value: 'value2'\n"
-                + "Property 'numbers2' was updated. From [complex value] to [complex value]\n"
-                + "Property 'numbers3' was removed\n"
-                + "Property 'numbers4' was added with value: [complex value]\n"
-                + "Property 'obj1' was added with value: [complex value]\n"
-                + "Property 'setting1' was updated. From 'Some value' to 'Another value'\n"
-                + "Property 'setting2' was updated. From 200 to 300\n"
-                + "Property 'setting3' was updated. From true to 'none'\n";
+        String expected = excepted3;
         String actual = Differ.generate("file1.json", "file2.json", "plain");
-        assertEquals(expected, actual);
+        System.out.println("Expected:\n" + expected);
+        System.out.println("Actual:\n" + actual);
+        assertEquals(expected.trim(), actual.trim());
+    }
+
+    @Test
+    void callJson() throws Exception {
+        String expected = excepted4;
+        String actual = Differ.generate("file1.json", "file2.json", "json");
+        assertEquals(expected.trim(), actual.trim());
     }
 
 
